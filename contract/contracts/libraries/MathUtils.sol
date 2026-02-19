e733237d34dd7c0ecca8cdd6d6327ff5ff61f72e
TrVHau <haulg3@gmail.com>
2026-02-19 23:52:41 +0700

    feat: implement MathUtils library with basic math functions and mulDivDown

--- contract/contracts/libraries/MathUtils.sol
+++ contract/contracts/libraries/MathUtils.sol
@@ -1,2 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
+
+library MathUtils {
+    uint256 internal constant WAD = 1e18;
+    uint256 internal constant RAY = 1e27;
+    uint256 internal constant BPS = 1e4;
+
+
+    // basic math utils
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
+
+    function max(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a > b ? a : b;
+    }
+
+    function clamp(uint256 x, uint256 lo, uint256 hi) internal pure returns (uint256) {
+        if(x < lo) return lo;
+        if(x > hi) return hi;
+        return min(max(x, lo), hi);
+    }
+
+    // div 
+
+    function mulDivDown(uint256 a, uint256 b, uint256 denominator) internal pure returns (uint256) {
+        require(denominator !=0, "DIV BY REZO");
+        
+    }
+}
